23d48e4da22a548e10a103c8fc94a173dace1841
Johnykutty <johnykutty@Johnykuttys-MacBook-Pro.local>
2014-08-29 22:48:06 +0530

    Added initial set of files

---
+++ JKBlockCommenter/JKJKBlockCommenter.h
@@ -0,0 +1,13 @@
+//
+//  JKJKBlockCommenter.h
+//  JKJKBlockCommenter
+//
+//  Created by Johnykutty on 29/08/14.
+//  Copyright (c) 2014 Johnykutty. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface JKJKBlockCommenter : NSObject
+
+@end
